5a699b5213dbbe4871e2297546e96c8903763273
Tu Beo <doanhtu278@gmail.com>
2015-04-05 10:04:21 +0700

    Logo
    Edit logo

--- parts/widgets/header/logo.php
+++ parts/widgets/header/logo.php
@@ -3,7 +3,7 @@
 	<a href="<?php echo BASE_URL;?>/index.php?page=home">
 		<!--<img alt="logo" src="assets/images/logo.svg" width="233" height="54"/>-->
 		<!--<object id="sp" type="image/svg+xml" data="assets/images/logo.svg" width="233" height="54"></object>-->
-		<img src="assets/images/Fresh Ideas_logo.jpg" width="230" height="69">
+		<img src="assets/images/Fresh Ideas_logo3.bmp" width="230" height="69">
 	</a>
 </div><!-- /.logo -->
 <!-- ============================================================= LOGO : END ============================================================= -->
